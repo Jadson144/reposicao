cd31cf057dd6a945659df88abc980c36fb95e4e5
Fernando Helton Linhares Soares <1060782@ifrn.local>
2024-01-10 09:21:22 -0300

    arquivos iniciais

---
+++ src/test/java/pi/reposicao/reposicao/ReposicaoApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.reposicao.reposicao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposicaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
